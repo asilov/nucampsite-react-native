537daf3be0410dc526fc9bc65e3dec232181a4a3
Asilbek Asilov <asil.california@gmail.com>
2022-09-15 15:24:05 -0700

    Social sharing

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { StyleSheet, Text, View, PanResponder, Alert } from "react-native";
+import { StyleSheet, Text, View, PanResponder, Alert, Share } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 import * as Animatable from "react-native-animatable";
@@ -51,47 +51,73 @@ const RenderCampsite = (props) => {
     },
   });
 
+  const shareCampsite = (title, message, url) => {
+    Share.share(
+			{
+				title,
+				message: `${title}: ${message} ${url}`,
+				url,
+			},
+			{
+				dialogTitle: 'Share ' + title,
+			}
+		)
+  }
+
   if (campsite) {
     return (
-      <Animatable.View
-        animation="fadeInDownBig"
-        duration={2000}
-        delay={1000}
-        ref={view}
-        {...panResponder.panHandlers}
-      >
-        <Card containerStyle={styles.cardContainer}>
-          <Card.Image source={{ uri: baseUrl + campsite.image }}>
-            <View style={{ justifyContent: "center", flex: 1 }}>
-              <Text style={styles.cardText}>{campsite.name}</Text>
-            </View>
-          </Card.Image>
-          <Text style={{ margin: 20 }}>{campsite.description}</Text>
-          <View style={styles.cardRow}>
-            <Icon
-              name={props.isFavorite ? "heart" : "heart-o"}
-              type="font-awesome"
-              color="#f50"
-              raised
-              reverse
-              onPress={() =>
-                props.isFavorite
-                  ? console.log("Already set as a favorite")
-                  : props.markFavorite()
-              }
-            />
-            <Icon
-              name="pencil"
-              type="font-awesome"
-              color="#5637DD"
-              raised
-              reverse
-              onPress={props.onShowModal}
-            />
-          </View>
-        </Card>
-      </Animatable.View>
-    );
+			<Animatable.View
+				animation='fadeInDownBig'
+				duration={2000}
+				delay={1000}
+				ref={view}
+				{...panResponder.panHandlers}>
+				<Card containerStyle={styles.cardContainer}>
+					<Card.Image source={{ uri: baseUrl + campsite.image }}>
+						<View style={{ justifyContent: 'center', flex: 1 }}>
+							<Text style={styles.cardText}>{campsite.name}</Text>
+						</View>
+					</Card.Image>
+					<Text style={{ margin: 20 }}>{campsite.description}</Text>
+					<View style={styles.cardRow}>
+						<Icon
+							name={props.isFavorite ? 'heart' : 'heart-o'}
+							type='font-awesome'
+							color='#f50'
+							raised
+							reverse
+							onPress={() =>
+								props.isFavorite
+									? console.log('Already set as a favorite')
+									: props.markFavorite()
+							}
+						/>
+						<Icon
+							name='pencil'
+							type='font-awesome'
+							color='#5637DD'
+							raised
+							reverse
+							onPress={props.onShowModal}
+						/>
+						<Icon
+							name='share'
+							type='font-awesome'
+							color='#5637DD'
+							raised
+							reverse
+							onPress={() =>
+								shareCampsite(
+									campsite.name,
+									campsite.description,
+									baseUrl + campsite.image
+								)
+							}
+						/>
+					</View>
+				</Card>
+			</Animatable.View>
+		)
   }
   return <View />;
 };
